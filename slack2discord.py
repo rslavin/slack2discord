223d215122a77ae66499b0e76ab191bfaf76e597
rslavin <rocky.slavin@gmail.com>
2020-06-05 20:27:57 -0500

    added @mention replacement; changed display names from real names

--- slack2discord.py
+++ slack2discord.py
@@ -32,6 +32,37 @@ def get_file_paths(file_path):
     return json_files
 
 
+def get_display_names(file_path):
+    """
+    Generates a dictionary of user_id => display_name pairs
+    :param file_path: Path to users.json
+    :return: Dictionary
+    """
+    users = {}
+    try:
+        with open(file_path) as f:
+            users_json = json.load(f)
+            for user in users_json:
+                users[user['id']] = (user['profile']['display_name'] if user['profile']['display_name'] else "UNKNOWN")
+                print(f"\tUser ID: {user['id']} -> Display Name: {users[user['id']]}")
+    except Exception as e:
+        print(f"[ERROR] Unable to load display names: {e}")
+        return None
+    return users
+
+
+def fill_mentions(message, users):
+    """
+    Fills in @mentions with their known display names
+    :param message:
+    :param users:
+    :return:
+    """
+    for uid, name in users.items():
+        message = message.replace(f"<@{uid}>", f"@{name}")
+    return message
+
+
 def register_commands():
     @bot.command(pass_context=True)
     async def import_here(ctx, *path):
@@ -43,10 +74,25 @@ def register_commands():
         :return:
         """
         path = ' '.join(path)
+        print(f"[INFO] Attempting to import '{path}' to channel '#{ctx.message.channel.name}'")
         json_file_paths = get_file_paths(path)
 
+        # attempt to find users.json to fill in @mentions
+        user_file_path_dir = os.path.join(os.path.dirname(json_file_paths[0]), "users.json")
+        user_file_path_files = os.path.join(os.path.dirname(os.path.dirname(json_file_paths[0])), "users.json")
+        users = {}
+        print(f"[INFO] Attempting to load users.json")
+        if os.path.isfile(user_file_path_dir):
+            users = get_display_names(user_file_path_dir)
+        elif os.path.isfile(user_file_path_files):
+            users = get_display_names(user_file_path_files)
+        if users:
+            print(f"[INFO] users.json found - attempting to fill @mentions")
+        else:
+            print(f"[WARNING] No users.json found - @mentions will contain user IDs instead of display names")
+
         if not json_file_paths:
-            print(f"[FAILED] No .json files found at {path}")
+            print(f"[ERROR] No .json files found at {path}")
         else:
             for json_file in sorted(json_file_paths):
                 print(f"[INFO] Parsing file: {json_file}")
@@ -54,18 +100,19 @@ def register_commands():
                     with open(json_file) as f:
                         for message in json.load(f):
                             if all(key in message for key in ['user_profile', 'ts', 'text']):
-                                username = (message['user_profile']['real_name'])
+                                username = (message['user_profile']['display_name'])
                                 timestamp = datetime.fromtimestamp(float(message['ts'])).strftime(
                                     '%m/%d/%Y at %H:%M:%S')
-                                text = (message['text'])
+                                text = fill_mentions(message['text'], users)
                                 msg = f"**{username}** *({timestamp})*\n{text}"
                                 await ctx.send(msg)
                                 print(f"[INFO] Imported message: '{msg}'")
                                 time.sleep(THROTTLE_TIME_SECONDS)
                             else:
-                                print("[ERROR] user information, timestamp, or message text missing")
+                                print("[WARNING] User information, timestamp, or message text missing")
                 except Exception as e:
                     print(f"[ERROR] {e}")
+            print(f"[INFO] Import complete")
 
 
 if __name__ == "__main__":
